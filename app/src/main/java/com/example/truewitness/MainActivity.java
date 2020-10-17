4f95aded6b73eae38fd85588433ea229f1297a1b
Peter <meirosep@yahoo.com>
2020-10-16 21:42:16 -0500

    Testing with Billian.

--- app/src/main/java/com/example/truewitness/MainActivity.java
+++ app/src/main/java/com/example/truewitness/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-//Test by Peter on 10/16/20 at 4:17pm.
+//Test by Peter on 10/16/20 at 9:41pm.
 
 public class MainActivity extends AppCompatActivity {
 
