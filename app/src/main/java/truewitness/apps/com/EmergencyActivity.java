cdbb2066462ba4ef92da25b7a1ae617371abaf4e
billian <billian.tapan@marquette.edu>
2020-10-19 21:17:54 -0500

    first commit

--- app/src/main/java/truewitness/apps/com/EmergencyActivity.java
+++ app/src/main/java/truewitness/apps/com/EmergencyActivity.java
@@ -17,28 +17,72 @@ import android.widget.Toast;
 
 
 public class EmergencyActivity extends AppCompatActivity {
-    private static int REQUEST_CALL = 1;
-    private TextView number1;
-    public Button button1;
+    private static final int REQUEST_CALL = 1;
+    private TextView number1,number2,number3;
+    public Button button1,button2,button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
-        number1 = findViewById(R.id.Emergencynum1);
-        button1 = findViewById(R.id.Emergencynum1ID);
+
+        number1 = findViewById(R.id.EmergencyNum1);
+        number2 = findViewById(R.id.EmergencyNum2);
+        number3 = findViewById(R.id.EmergencyNum3);
+        button1 = findViewById(R.id.EmergencyNum1ID);
+        button2 = findViewById(R.id.EmergencyNum2ID);
+        button3 = findViewById(R.id.EmergencyNum3ID);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                callButton();
+                callButton1();
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                callButton2();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                callButton3();
+            }
+        });
     }
 
-    public void callButton() {
+    public void callButton1() {
         String number = number1.getText().toString();
+        if (number.trim().length() > 0) {
+            if (ContextCompat.checkSelfPermission(EmergencyActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(EmergencyActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+            } else {
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+
+
+            }
+        }
+    }
+    public void callButton2() {
+        String number = number2.getText().toString();
+        if (number.trim().length() > 0) {
+            if (ContextCompat.checkSelfPermission(EmergencyActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(EmergencyActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+            } else {
+                String dial = "tel:" + number;
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+
+
+            }
+        }
+    }
+    public void callButton3() {
+        String number = number3.getText().toString();
         if (number.trim().length() > 0) {
             if (ContextCompat.checkSelfPermission(EmergencyActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(EmergencyActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
@@ -55,7 +99,9 @@ public class EmergencyActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == REQUEST_CALL) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                callButton();
+                callButton1();
+                callButton2();
+                callButton3();
 
             } else {
                 Toast.makeText(this, "permission DENIED", Toast.LENGTH_SHORT).show();
